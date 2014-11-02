3363e12e15bdf27de6610246eb5e6bff3dd7e3bc
Brian Olson <brian.olson@gmail.com>
2014-11-01 21:04:10 -0400

    add a little package documentation

---
+++ go/doc.go
@@ -0,0 +1,11 @@
+/*
+CBOR is IETF RFC 7049, the "Concise Binary Object Representation"
+http://tools.ietf.org/html/rfc7049
+
+In can be thought of as "binary JSON" but is a superset and somewhat richer representation than JSON.
+
+Other implementations and more information can also be found at:
+http://cbor.io/
+
+*/
+package cbor
